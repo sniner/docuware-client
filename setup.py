d8bd532cff5f1b1a9708b09bc91ec77e6b4a0098
Stefan Schönberger <mail@sniner.dev>
2023-06-30 17:51:12 +0200

    Bumped version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "docuware-client",
-    version = "0.1.0",
+    version = "0.2.0",
     description = "DocuWare REST-API client",
     long_description = open("README.md").read(),
     long_description_content_type = "text/markdown",
